4a6acb42458aa5831fa82f81c738661df76f924b
Jay <jamied_uk@hotmail.com>
2025-11-21 09:15:20 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # (c) J~Net 2025
 #
+mkdir -p output
+
 python -m venv venv
 source venv/bin/activate
 echo "Virtual Environment Setup and ready!"
@@ -8,3 +10,5 @@ echo "Virtual Environment Setup and ready!"
 pip install -r requirements.txt
 pip install --upgrade pip
 pip install requests beautifulsoup4
+
+echo "Now ready to run ./start.sh website.com"
